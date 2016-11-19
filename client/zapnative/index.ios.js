58dbc70e1b6a2155f08c1ffd0134e162999a7724
tetriseffect <avakyan@gmail.com>
2016-11-19 08:39:40 -0800

    Added ignite mobile start app

---
+++ client/zapnative/index.ios.js
@@ -0,0 +1,15 @@
+import './App/Config/ReactotronConfig'
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import Root from './App/Root'
+import configureStore from './App/Store/Store'
+
+// Handling store here to avoid hot-reloading issues
+const store = configureStore()
+class RNBase extends React.Component {
+  render () {
+    return <Root {...this.props} store={store} />
+  }
+}
+
+AppRegistry.registerComponent('zapnative', () => RNBase)
